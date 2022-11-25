d8a1262705a1489eb1a6f501e0a983f577511efc
Rene Sorger <rene_sorger@gmx.at>
2022-11-25 13:44:56 +0100

    Created Datareader

---
+++ SoftwareArchitektur.ExcelDataReader/DataHolder.cs
@@ -0,0 +1,10 @@
+﻿using SoftwareArchitektur.Utility.Models;
+
+namespace SoftwareArchitektur.ExcelDataReader;
+
+public class DataHolder
+{
+    public static readonly List<Service> ServiceList= new List<Service>();
+    public static readonly List<DependencyRelation> DependencyList= new List<DependencyRelation>();
+    public static readonly List<CommonChangeRelation> ChangedWithList= new List<CommonChangeRelation>();
+}
---
+++ SoftwareArchitektur.Utility/Models/DependencyRelation.cs
@@ -0,0 +1,8 @@
+﻿namespace SoftwareArchitektur.Utility.Models;
+
+public class DependencyRelation
+{
+    public string Caller = String.Empty;
+    public string Callee = String.Empty;
+    public long NumberOfCalls = 0;
+}
